54efb03616a0644573bcb97358f5a300630d4e96
DomasPauliukas <Domas0319@gmail.com>
2025-04-30 12:59:03 +0300

    .

---
+++ db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+const process = require('process')
+
+mongoose.connect(process.env.DB_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((error) => console.error('MongoDB connection error:', error))
+
+process.on('SIGINT', () => {
+  mongoose.connection.close(() => {
+    console.log('MongoDB disconnected')
+    process.exit(0)
+  })
+})
